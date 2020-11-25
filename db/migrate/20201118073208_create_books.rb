32940b438ca20d79e2abc07d5f312f4d67ceb675
Ubuntu <ubuntu@ip-172-31-79-91.ec2.internal>
2020-11-25 08:02:38 +0000

    Add detail book

--- db/migrate/20201118073208_create_books.rb
+++ db/migrate/20201118073208_create_books.rb
@@ -3,6 +3,7 @@ class CreateBooks < ActiveRecord::Migration[6.0]
     create_table :books do |t|
       t.string :title
       t.integer :number_of_pages
+      t.text :detail
 
       t.timestamps
     end
